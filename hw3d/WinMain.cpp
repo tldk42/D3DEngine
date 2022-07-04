076a2dca9ccfeada388603155de929a5daf2d6d1
Jacob <tldk47@hanmail.net>
2022-07-04 19:27:30 +0900

    Message Loop 설정

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -1,5 +1,17 @@
 #include <Windows.h>
 
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch(msg)
+	{
+	case WM_CLOSE :
+		PostQuitMessage(69);
+		break;
+
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
@@ -11,7 +23,7 @@ int CALLBACK WinMain(
 	WNDCLASSEX wc = {0};
 	wc.cbSize = sizeof(wc);
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
+	wc.lpfnWndProc = WndProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
@@ -28,11 +40,29 @@ int CALLBACK WinMain(
 		0, pClassName,
 		"Happy Hard Window",
 		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
-		200, 200, 640, 480,
+		200, 200, 1080, 920,
 		nullptr, nullptr, hInstance, nullptr);
 
+	// Show the Damn window
 	ShowWindow(hWnd, SW_SHOW);
-	while (true);
+
+	// Message pump
+	MSG msg;
+	BOOL gResult;
+	while ((gResult = GetMessage(&msg, nullptr, 0, 0) > 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	if (gResult == -1)
+	{
+		return -1;
+	}
+	else
+	{
+		return msg.wParam;
+	}
 
 	return 0;
 }
