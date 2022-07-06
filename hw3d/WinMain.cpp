44adc461d12ee54a361c71b516517d664d4da13b
Jacob <tldk47@hanmail.net>
2022-07-06 11:29:28 +0900

    Window Framework

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -1,91 +1,21 @@
-#include <Windows.h>
-#include <sstream>
-#include "WindowsMessageMap.h"
+#include "Window.h"
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	static WindowsMessageMap mm;
-	OutputDebugString(mm(msg, lParam, wParam).c_str());
-	switch (msg)
-	{
-	case WM_CLOSE:
-		PostQuitMessage(69);
-		break;
-	case WM_KEYDOWN:
-		if (wParam == 'F') // testing
-		{
-			SetWindowText(hWnd, "Title Changed");
-		}
-		break;
-	case WM_CHAR:
-		{
-			static std::string title;
-			title.push_back(static_cast<char>(wParam));
-			SetWindowText(hWnd, title.c_str());
-		}
-		break;
-	case WM_LBUTTONDOWN:
-		{
-			POINTS pt = MAKEPOINTS(lParam);
-			std::ostringstream oss;
-			oss << "(" << pt.x << "," << pt.y << ")";
-			SetWindowText(hWnd, oss.str().c_str());
-		}
-	}
-	return DefWindowProc(hWnd, msg, wParam, lParam);
-}
+	Window wnd(800, 400, "Donkey Dart Box");
 
-int CALLBACK WinMain(
-	HINSTANCE hInstance,
-	HINSTANCE hPrevInstance,
-	LPSTR lpCmdLine,
-	int mCmdShow)
-{
-	const auto pClassName = "hw3dbutts";
-	// register window class
-	WNDCLASSEX wc = {0};
-	wc.cbSize = sizeof(wc);
-	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = WndProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = nullptr;
-	wc.hCursor = nullptr;
-	wc.hIconSm = nullptr;
-	wc.hbrBackground = nullptr;
-	wc.lpszMenuName = nullptr;
-	wc.lpszClassName = pClassName;
-	RegisterClassEx(&wc);
-
-	// create window
-	HWND hWnd = CreateWindowEx(
-		0, pClassName,
-		"Sample Window",
-		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
-		200, 200, 1080, 920,
-		nullptr, nullptr, hInstance, nullptr);
-
-	// Show the Damn window
-	ShowWindow(hWnd, SW_SHOW);
-
-	// Message pump
 	MSG msg;
 	BOOL gResult;
-	while ((gResult = GetMessage(&msg, nullptr, 0, 0) > 0))
+	while((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
 	{
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
 
-	if (gResult == -1)
+	if (gResult == - 1)
 	{
 		return -1;
 	}
-	else
-	{
-		return msg.wParam;
-	}
 
-	return 0;
+	return msg.wParam;
 }
